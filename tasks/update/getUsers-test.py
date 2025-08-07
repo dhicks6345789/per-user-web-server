a94d605a18bf83037e3b6dc1e973a424b4b86dbe
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2025-08-07 19:00:41 +0100

    Update getUsers-test.py

--- tasks/update/getUsers-test.py
+++ tasks/update/getUsers-test.py
@@ -1,8 +1,12 @@
+import os
 import csv
 
 print("Generating test data.", flush=True)
 
-with open("users.csv", "w", newline="\n") as usersFile:
+if not os.path.exists("data"):
+    os.makedirs("data")
+
+with open("data/users.csv", "w", newline="\n") as usersFile:
     usersWriter = csv.writer(usersFile, delimiter=",", quotechar="\"", quoting=csv.QUOTE_ALL)
     usersWriter.writerow(["User One", "user1"])
     usersWriter.writerow(["User Two", "user2"])

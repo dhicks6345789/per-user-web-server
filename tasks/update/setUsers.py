60db9764ad66aec5486d83aed7c3261400b752e2
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2025-08-07 19:02:21 +0100

    Update setUsers.py

--- tasks/update/setUsers.py
+++ tasks/update/setUsers.py
@@ -1,8 +1,5 @@
-import csv
+import pandas
 
 print("Setting users in Pangolin...", flush=True)
 
-with open("users.csv", newline="\n") as usersFile:
-    usersReader = csv.reader(usersFile, delimiter=",", quotechar="\"")
-    for user in usersReader:
-        print(", ".join(user), flush=True)
+users = pandas.read_csv("data/users.csv")

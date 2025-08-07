7cb8d2c1fa3487f76518852e2745e383d8e6e8f5
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2025-08-07 18:46:10 +0100

    Update getUsers-test.py

--- tasks/update/getUsers-test.py
+++ tasks/update/getUsers-test.py
@@ -1,3 +1,9 @@
 import csv
 
 print("Generating test data.", flush=True)
+
+with open("users.csv", "w", newline="\n") as usersFile:
+    usersWriter = csv.writer(usersFile, delimiter=",", quotechar="\"", quoting=csv.QUOTE_ALL)
+    usersWriter.writerow(["User One", "user1"])
+    usersWriter.writerow(["User Two", "user2"])
+    usersWriter.writerow(["User Three", "user3"])

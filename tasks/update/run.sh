89ddc5070a3d61871308f0cf85689db8116a8833
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2025-08-07 18:38:32 +0100

    Update run.sh

--- tasks/update/run.sh
+++ tasks/update/run.sh
@@ -1,1 +1,5 @@
-python3 run.py
+# The default getUsers script - generates "user1", "user2" and "user3" test data.
+python3 getUsers-test.py
+
+# Run the script that uses the user data to create users in Pangolin.
+python3 setUsers.py

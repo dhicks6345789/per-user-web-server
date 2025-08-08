ea6eb0be7ac10c3670b2b97bfc309583433395c6
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2025-08-08 10:27:14 +0100

    Update setUsers.py

--- tasks/update/setUsers.py
+++ tasks/update/setUsers.py
@@ -2,4 +2,6 @@ import pandas
 
 print("Setting users in Pangolin...", flush=True)
 
-users = pandas.read_csv("data/users.csv")
+users = pandas.read_csv("data/users.csv", header=0)
+for userIndex, userRow in users.iterrows():
+    print(userRow, flush=True)

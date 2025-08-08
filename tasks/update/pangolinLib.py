f024f17a422c8b8e5bdf3d3dcccf4ad350cefef1
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2025-08-08 17:23:24 +0100

    Update pangolinLib.py

--- tasks/update/pangolinLib.py
+++ tasks/update/pangolinLib.py
@@ -58,7 +58,7 @@ def APICall(theAPIKey, theAPIBaseURL, theAPIURL):
 			time.sleep(retrySeconds);
 		else:
 			if APIResponse.status_code != 200:
-				print(APIResponse.reason, flush=True)
+				print(APIResponse.headers, flush=True)
 				print("ERROR: Pangolin API return code: " + str(APIResponse.status_code), flush=True)
 				exit()
 			return APIResponse.json()

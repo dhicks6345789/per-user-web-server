59c30c26d054133bb6600fe64827b54199f270f4
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2025-08-08 16:48:03 +0100

    Update pangolinLib.py

--- tasks/update/pangolinLib.py
+++ tasks/update/pangolinLib.py
@@ -4,3 +4,38 @@ def readFile(theFilename):
 	result = inHandle.read()
 	inHandle.close()
 	return result
+
+# Takes an array of strings, then checks the JSON config file to make sure the required parameters are indeed set.
+# Returns an array of configuration values.
+def loadConfig(configFile="config.json", requiredParameters=[]):
+	# Load the configuration file.
+	config = json.loads(readFile(configFile))
+	for requiredParameter in requiredParameters:
+		if not requiredParameter in config.keys():
+			print("Error - required value " + requiredParameter + " not set in " + configFile + ".", flush=True)
+			sys.exit(1)
+	return config
+    
+# A function to call the Pangolin API. Handles "Retry-After" errors by pausing
+# and retrying after the defined delay, but simply exits on all other errors.
+def APICall(theAPIKey, theAPIBaseURL, theAPIURL):
+	# A small delay so we don't hit the API rate limit.
+	time.sleep(1)
+	
+	APIURL = theAPIURL
+	if not APIURL.startswith(theAPIBaseURL):
+		APIURL = theAPIBaseURL + APIURL
+
+	retries = 0
+	while retries < 2:
+		APIResponse = requests.get(APIURL, headers = {"Authorization": "token " + theAPIKey})
+		if APIResponse.status_code == 429:
+			retrySeconds = int(APIResponse.headers["Retry-After"])
+			print("WARNING: Pangolin API rate limit hit - pausing for " + str(retrySeconds) + " seconds.", flush=True)
+			time.sleep(retrySeconds);
+		else:
+			if APIResponse.status_code != 200:
+				print("ERROR: Pangolin API return code: " + str(APIResponse.status_code), flush=True)
+				exit()
+			return APIResponse.json()
+		retries = retries + 1

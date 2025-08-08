fc400af5340dc7297526872829bb87e38f3fa260
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2025-08-08 10:24:52 +0100

    Update run.sh

--- tasks/update/run.sh
+++ tasks/update/run.sh
@@ -1,11 +1,3 @@
-# Pandas - Python library for handling data, also Excel files.
-#if [ ! -e /usr/share/doc/python3-pandas ]; then
-#  apt-get update && apt-get install -y python3-pandas
-#fi
-#export PYTHONPATH="/usr/lib/python3/dist-packages/:/usr/lib/python3.11/dist-packages/"
-
-
-
 # The default getUsers script - generates "user1", "user2" and "user3" test data.
 python3 getUsers-test.py
 

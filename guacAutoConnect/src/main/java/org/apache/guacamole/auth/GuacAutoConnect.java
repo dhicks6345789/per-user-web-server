84e484c2d6c0840d00098f4ca07895172c99df09
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-01-18 14:59:59 +0000

    Update GuacAutoConnect.java

--- guacAutoConnect/src/main/java/org/apache/guacamole/auth/GuacAutoConnect.java
+++ guacAutoConnect/src/main/java/org/apache/guacamole/auth/GuacAutoConnect.java
@@ -76,6 +76,7 @@ public class GuacAutoConnect extends SimpleAuthenticationProvider {
         // If we've run out of available ports, don't start a new instance.
         if (vncDisplay <= 20) {
           logger.info("Starting a new desktop instance for user " + username + " on port " + desktopPort);
+          logger.info("docker", "run", "--detach", "--name", "desktop-" + username, "sansay.co.uk-dockerdesktop:0.1-beta.3", "vncserver", "-fg", "-localhost", "no", "-geometry", "1280x720", ":" + String.valueOf(vncDisplay));
           processBuilder = new ProcessBuilder("docker", "run", "--detach", "--name", "desktop-" + username, "sansay.co.uk-dockerdesktop:0.1-beta.3", "vncserver", "-fg", "-localhost", "no", "-geometry", "1280x720", ":" + String.valueOf(vncDisplay));
           try {
             Process process = processBuilder.start();

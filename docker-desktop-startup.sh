2d6df236ebaf08d5850253ee89ff798b5340325f
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-01-18 16:00:11 +0000

    Create docker-desktop-startup.sh

---
+++ docker-desktop-startup.sh
@@ -0,0 +1,8 @@
+echo "First Argument: $1"
+echo "Second Argument: $2"
+
+# Set up VNC password.
+mkdir -p /home/desktopuser/.vnc && echo "vncpassword" | vncpasswd -f > /home/desktopuser/.vnc/passwd && chmod 600 /home/desktopuser/.vnc/passwd
+
+# Start TigerVNC.
+vncserver -fg -localhost no -geometry 1280x720 :1

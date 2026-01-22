f8e69d3dc059bfe211e7051832290a25587a03d9
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-01-22 23:30:38 +0000

    Update GuacAutoConnect.java

--- guacAutoConnect/src/main/java/org/apache/guacamole/auth/GuacAutoConnect.java
+++ guacAutoConnect/src/main/java/org/apache/guacamole/auth/GuacAutoConnect.java
@@ -95,9 +95,11 @@ public class GuacAutoConnect extends SimpleAuthenticationProvider {
     // 2. Convert Map to form-url-encoded string
     String form = sessionParameters.entrySet().stream().map(e -> e.getKey() + "=" + e.getValue()).collect(Collectors.joining("&"));
     logger.info("form: " + form);
+    
 
     HttpClient client = HttpClient.newHttpClient();
-    HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://httpbin.org/post")).header("Content-Type", "application/x-www-form-urlencoded").POST(BodyPublishers.ofString(form)).build();
+    //HttpRequest request = HttpRequest.newBuilder().uri(URI.create("http://host.docker.internal:8091/connectOrStartSession")).header("Content-Type", "application/x-www-form-urlencoded").POST(BodyPublishers.ofString(form)).build();
+    HttpRequest request = HttpRequest.newBuilder().uri(URI.create("http://host.docker.internal:8091/connectOrStartSession")).header("Content-Type", "application/x-www-form-urlencoded").POST("{\"username\": \"" + username + "\"}").build();
     
     try {
       HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());

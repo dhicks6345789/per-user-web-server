3f2e32de8e2e47f8dde37ea70e4cab6f9efebfd3
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-01-22 15:21:05 +0000

    Update sessionManager.go

--- sessionManager/sessionManager.go
+++ sessionManager/sessionManager.go
@@ -18,43 +18,6 @@ func main() {
 	}
 	defer cli.Close()
 
-	// 1. Endpoint to List Containers
-	//http.HandleFunc("/containers", func(w http.ResponseWriter, r *http.Request) {
-		//fmt.Println("Called container.")
-		//containers, err := cli.ContainerList(context.Background(), client.ContainerListOptions{})
-		//if err != nil {
-			//http.Error(w, err.Error(), http.StatusInternalServerError)
-			//return
-		//}
-
-		//w.Header().Set("Content-Type", "application/json")
-		//json.NewEncoder(w).Encode(containers)
-	//})
-
-	// 2. Endpoint to Stop a Container
-	// Usage: POST /stop?id=container_id_here
-	//http.HandleFunc("/stop", func(w http.ResponseWriter, r *http.Request) {
-		//if r.Method != http.MethodPost {
-			//http.Error(w, "Only POST is allowed", http.StatusMethodNotAllowed)
-			//return
-		//}
-
-		//containerID := r.URL.Query().Get("id")
-		//if containerID == "" {
-			//http.Error(w, "Missing 'id' parameter", http.StatusBadRequest)
-			//return
-		//}
-
-		// Stop the container with a default timeout
-		//_, err := cli.ContainerStop(context.Background(), containerID, client.ContainerStopOptions{})
-		//if err != nil {
-			//http.Error(w, fmt.Sprintf("Failed to stop: %v", err), http.StatusInternalServerError)
-			//return
-		//}
-
-		//fmt.Fprintf(w, "Container %s stopped successfully", containerID)
-	})
-
 	// Endpoint connectOrStartSession - returns a port number and password to connect with VNC.
 	// Usage: POST /connectOrStartSession?username=USERNAME
 	// Returns: JSON {errorCode, portNumber, password}
@@ -67,6 +30,14 @@ func main() {
 		}
 
 		fmt.Println("Looking for session for user: ", username)
+		
+		//containers, err := cli.ContainerList(context.Background(), client.ContainerListOptions{})
+		//if err != nil {
+			//http.Error(w, err.Error(), http.StatusInternalServerError)
+			//return
+		//}
+		//w.Header().Set("Content-Type", "application/json")
+		//json.NewEncoder(w).Encode(containers)
 	}
 
 	fmt.Println("Server starting on :8091...")

8a5d91701d02f725ea508b1c734ba6e53ed65d13
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-01-22 15:36:58 +0000

    Update sessionManager.go

--- sessionManager/sessionManager.go
+++ sessionManager/sessionManager.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	//"context"
-	//"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -20,24 +18,26 @@ func main() {
 
 	// Endpoint connectOrStartSession - returns a port number and password to connect with VNC.
 	// Usage: POST /connectOrStartSession?username=USERNAME
-	// Returns: JSON {errorCode, portNumber, password}
+	// Returns: JSON { errorCode, portNumber, password }
 	// If an existing session already exists for the user it returns the details for that, otherwise it starts a new desktop session (container).
 	http.HandleFunc("/connectOrStartSession", func(w http.ResponseWriter, r *http.Request) {
-		username := r.URL.Query().Get("username")
+		username := strings.TrimSpace(r.URL.Query().Get("username"))
 		if username == "" {
 			http.Error(w, "Missing 'username' parameter", http.StatusBadRequest)
 			return
 		}
 
 		fmt.Println("Looking for session for user: ", username)
-		
+
 		//containers, err := cli.ContainerList(context.Background(), client.ContainerListOptions{})
 		//if err != nil {
 			//http.Error(w, err.Error(), http.StatusInternalServerError)
 			//return
 		//}
-		//w.Header().Set("Content-Type", "application/json")
-		//json.NewEncoder(w).Encode(containers)
+		
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprintf(w, "{\"errorCode\":\"\", \"portNumber\":\"\", \"password\":\"\"}")
+    }
 	})
 
 	fmt.Println("Server starting on :8091...")

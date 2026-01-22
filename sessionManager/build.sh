ea6ba6aa6dd21203952c2d5cc366dbbd23fb51ed
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-01-22 14:22:58 +0000

    Update build.sh

--- sessionManager/build.sh
+++ sessionManager/build.sh
@@ -12,12 +12,17 @@ go get github.com/moby/moby/client
 # Clear out any previously-compile binary.
 rm sessionManager
 
+# Build the executable.
 go build sessionManager.go
-#cp sessionManager /usr/local/bin
+
+# Exit if we didn't manage to build the executable.
+[ ! -f sessionManager ] && exit
+
+cp sessionManager /usr/local/bin
 
 # Set up systemd to run PUWSSessionManager, if it isn't already.
-#[ ! -f /etc/systemd/system/webconsole.service ] && cp webconsole.service /etc/systemd/system/webconsole.service && chmod 644 /etc/systemd/system/webconsole.service
+[ ! -f /etc/systemd/system/PUWSSessionManager.service ] && cp PUWSSessionManager.service /etc/systemd/system/PUWSSessionManager.service && chmod 644 /etc/systemd/system/PUWSSessionManager.service
 
 # Restart the PUWSSessionManager service.
-#systemctl start PUWSSessionManager
-#systemctl enable PUWSSessionManager
+systemctl start PUWSSessionManager
+systemctl enable PUWSSessionManager

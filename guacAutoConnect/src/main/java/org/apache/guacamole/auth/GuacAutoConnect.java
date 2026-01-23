39cccc780b734bf4282aa346131e3612f99295f1
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-01-23 08:53:45 +0000

    Update GuacAutoConnect.java

--- guacAutoConnect/src/main/java/org/apache/guacamole/auth/GuacAutoConnect.java
+++ guacAutoConnect/src/main/java/org/apache/guacamole/auth/GuacAutoConnect.java
@@ -15,6 +15,7 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.net.http.HttpRequest.BodyPublishers;
 
 import org.apache.guacamole.GuacamoleException;
 import org.apache.guacamole.net.auth.simple.SimpleAuthenticationProvider;
@@ -24,56 +25,19 @@ import org.apache.guacamole.protocol.GuacamoleConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-// Temporary?
-import java.util.stream.Collectors;
-import java.net.http.HttpRequest.BodyPublishers;
-
 /**
- * A custom authentication provider that automatically connects the given user to a remote desktop instance hosted inside a Docker container.
+ * A custom Guacamole authentication provider that automatically connects the given user to a remote desktop instance hosted inside a Docker container.
  * If a suitible instance matching the username doesn't already exist, one will be created.
  * This extension assumes something else is handling the actual authentication, and that by the time this code gets called the user is assumed to be valid and authorised.
  * Designed for use with Pangolin handling authentication and a VNC-enabled Desktop image available in Docker.
+ * Also, designed to communicate (via a simple HTTP API) with a dedicated Session Manager serivce running on the Docker host. This component itself runs inisde a container,
+ * it can't directly create sibling containers, it has to pass the request up to its host for that.
  */
 public class GuacAutoConnect extends SimpleAuthenticationProvider {
   // Initialize the logger for this class.
   private static final Logger logger = LoggerFactory.getLogger(GuacAutoConnect.class);
-
-  // A helper function that runs an external command, returning all the output produced as an array of Strings.
-  private static String[] runCommand(String... parameters) {
-    List<String> output = new ArrayList<>();
-    
-    // Log a message to the console.
-    logger.info("Running command: " + String.join(" ", parameters));
-    
-    try {
-      // Initialize ProcessBuilder with the command and its arguments.
-      ProcessBuilder processBuilder = new ProcessBuilder(parameters);
-    
-      // Redirect the command's error stream to standard output so we capture everything.
-      processBuilder.redirectErrorStream(true);
-
-      Process process = processBuilder.start();
-    
-      // Use try-with-resources to ensure the reader closes automatically
-      try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
-        String line;
-        while ((line = reader.readLine()) != null) {
-          output.add(line);
-        }
-      }
-    
-      // Wait for the process to finish and check the exit code
-      int exitCode = process.waitFor();
-      if (exitCode != 0) {
-        return new String[] {"ERROR: " + exitCode};
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-    return output.toArray(new String[0]);
-  }
   
-  // Tell Guacamole what the name of this custom extension is.
+  // Tell Guacamole what the name of this custom Guacamole extension is.
   @Override public String getIdentifier() {
     return "guac-auto-connect";
   }
@@ -88,41 +52,13 @@ public class GuacAutoConnect extends SimpleAuthenticationProvider {
     
     // Output a log message. We simply write to STDOUT, where the output can be displayed by Docker.
     logger.info("User " + username + " logged in.");
-    
-    Map<String, String> sessionParameters = new HashMap<>();
-    sessionParameters.put("username", username);
-    // 2. Convert Map to form-url-encoded string
-    String form = sessionParameters.entrySet().stream().map(e -> e.getKey() + "=" + e.getValue()).collect(Collectors.joining("&"));
-    logger.info("form: " + form);
     HttpClient client = HttpClient.newHttpClient();
-    //HttpRequest request = HttpRequest.newBuilder().uri(URI.create("http://host.docker.internal:8091/connectOrStartSession")).header("Content-Type", "application/x-www-form-urlencoded").POST(BodyPublishers.ofString(form)).build();
     HttpRequest request = HttpRequest.newBuilder().uri(URI.create("http://host.docker.internal:8091/connectOrStartSession")).header("Content-Type", "application/x-www-form-urlencoded").POST(BodyPublishers.ofString("username=" + username)).build();
     
     try {
       HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-      logger.info("Status Code: " + response.statusCode());
       logger.info("Response: " + response.body());
       String desktopPort = "5901";
-    
-      /*
-      // To do: unmount or re-use any existing user mount, make sure we don't double-up.
-      // Mount the user's Google Drive home to /mnt in the container host, ready to be passed to the user's desktop container.
-      String[] rcloneMountResult = runCommand("rclone", "mount", "gdrive:", "/mnt/" + username, "--allow-other", "--vfs-cache-mode", "writes", "--drive-impersonate", username + "@knightsbridgeschool.com", "&");
-      logger.info(String.join("\n", rcloneMountResult));
-    
-      logger.info("Starting a new desktop instance for user " + username + " on port " + desktopPort);
-      String[] dockerRunResult = runCommand("sudo", "docker", "run", "--detach", "--name", "desktop-" + username, "--expose", desktopPort, "--network", "pangolin_main", "sansay.co.uk-dockerdesktop:0.1-beta.3", "bash", "/home/desktopuser/startup.sh", "bananas", String.valueOf(vncDisplay));
-      logger.info(String.join("\n", dockerRunResult));
-    
-      // Wait for the desktop instance to start up before we do anything more.
-      // To do: maybe actually run docker ps -a more rather than just do a simple pause.
-      try {
-        Thread.sleep(2000);
-      } catch (InterruptedException e) {
-        Thread.currentThread().interrupt();
-        // Gemini: Handle the exception (e.g., logging).
-      }
-      */
       
       if (desktopPort.equals("")) {
         logger.info("Problem finding / starting desktop instance for user " + username);

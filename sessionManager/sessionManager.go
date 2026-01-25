efff3f0275127312be4c32a0563d087fcecbffc4
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-01-25 16:39:13 +0000

    Update sessionManager.go

--- sessionManager/sessionManager.go
+++ sessionManager/sessionManager.go
@@ -1,3 +1,6 @@
+/*
+	The session manager for the per-user-web-server (PUWS) project. Sits on the main host server and communicates with / manages Docker containers, in particular the desktop instances of users who are connecting via Guacamole.
+*/
 package main
 
 import (
@@ -18,6 +21,32 @@ import (
 )
 
 func main() {
+	// We want each desktop instance to have a separate, un-guessable VNC password. However, we also want that password to be consistant so we can easily reconnect a user to their session.
+	// Rather than hold session password in memory, we use a hash function to generate a password for each session from the username, port number and a secret seed value.
+	// That seed value is a simple string, stored in a text file at /etc/puws/seed.txt. If that path doesn't already exist, we create it now.
+	seedPath := "/etc/puws/seed.txt"
+	seedDir := filepath.Dir(seedPath)
+	seedDirErr := os.MkdirAll(dir, 0755)
+	if seedDirErr != nil {
+		fmt.Println("Error creating directories: " + seedDirErr.Error())
+		return
+	}
+
+	// Check if the seed value file exists, creating it if not.
+	_, seedFileErr := os.Stat(seedPath)
+	if os.IsNotExist(seedFileErr) {
+		newSeedFile, seedFileCreateErr := os.Create(seedPath)
+		if seedFileCreateErr != nil {
+			fmt.Println("Failed to create file: " + seedPath + ", " + seedFileCreateError.Error())
+			return
+		}
+		fmt.Fprintf(newSeedFile, "vncpassword")
+		newSeedFile.Close()
+	} else if seedFileErr != nil {
+		fmt.Println("An error occurred while checking the file: " + seedPath + ", " + seedFileError.Error())
+		return
+	}
+	
 	// Initialize the Docker client. It automatically looks for the Docker socket (unix:///var/run/docker.sock).
 	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
@@ -29,16 +58,16 @@ func main() {
 	// Usage: POST /connectOrStartSession?username=USERNAME
 	// Returns: JSON { portNumber, password }
 	// If an existing session already exists for the user it returns the details for that, otherwise it starts a new desktop session (container).
-	http.HandleFunc("/connectOrStartSession", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/connectOrStartSession", func(httpResponse http.ResponseWriter, r *http.Request) {
 		// Parse the HTTP GET/POST request form data.
 		if err := r.ParseForm(); err != nil {
-			http.Error(w, "Error parsing form", http.StatusBadRequest)
+			http.Error(httpResponse, "Error parsing form", http.StatusBadRequest)
 			return
 		}
 		// Get any passed variables using FormValue or PostForm.
 		username := strings.TrimSpace(r.FormValue("username"))
 		if username == "" {
-			http.Error(w, "Missing 'username' parameter", http.StatusBadRequest)
+			http.Error(httpResponse, "Missing 'username' parameter", http.StatusBadRequest)
 			return
 		}
 
@@ -47,7 +76,7 @@ func main() {
 		// Get a list of existing containers from Docker.
 		containers, err := cli.ContainerList(context.Background(), client.ContainerListOptions{})
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+			http.Error(httpResponse, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
@@ -78,7 +107,7 @@ func main() {
 			}
 			// If no free port found, return an error.
 			if possibleVNCPort == 0 {
-				http.Error(w, "No free sessions.", http.StatusInternalServerError)
+				http.Error(httpResponse, "No free sessions.", http.StatusInternalServerError)
 				return
 			}
 			VNCPort = possibleVNCPort
@@ -111,21 +140,21 @@ func main() {
 			})
 			// Check the container create process worked okay.
 			if containerCreateErr != nil {
-				http.Error(w, "Error creating container for user " + username + ", " + containerCreateErr.Error(), http.StatusInternalServerError)
+				http.Error(httpResponse, "Error creating container for user " + username + ", " + containerCreateErr.Error(), http.StatusInternalServerError)
 				return
 			}
 
 			// Start the newly-create container, report any errors.
 			_, containerStartErr := cli.ContainerStart(containerContext, resp.ID, client.ContainerStartOptions{})
 			if containerStartErr != nil {
-				http.Error(w, "Error starting container for user " + username + ", " + containerStartErr.Error(), http.StatusInternalServerError)
+				http.Error(httpResponse, "Error starting container for user " + username + ", " + containerStartErr.Error(), http.StatusInternalServerError)
 				return
 			}
 			
 			// Get a reader object to read the container logs so we can check to see when the VNC server has started up.
 			logReader, logReaderErr := cli.ContainerLogs(containerContext, resp.ID, client.ContainerLogsOptions{ShowStdout:true, ShowStderr:true, Follow:true, Timestamps:true, Tail:"all"})
 			if logReaderErr != nil {
-				http.Error(w, "Error getting reader from container, " + err.Error(), http.StatusInternalServerError)
+				http.Error(httpResponse, "Error getting reader from container, " + err.Error(), http.StatusInternalServerError)
 				return
 			}
 			defer logReader.Close()
@@ -142,14 +171,14 @@ func main() {
 			
 			// Report any errors during the log reading process.
 			if logScannerErr := logScanner.Err(); logScannerErr != nil {
-				http.Error(w, "Error getting reader from container, " + logScannerErr.Error(), http.StatusInternalServerError)
+				http.Error(httpResponse, "Error getting reader from container, " + logScannerErr.Error(), http.StatusInternalServerError)
 				return
 			}
 		}
 		
 		w.Header().Set("Content-Type", "application/json")
 		fmt.Printf("{\"portNumber\":\"%d\", \"password\":\"vncpassword\"}", VNCPort)
-		fmt.Fprintf(w, "{\"portNumber\":\"%d\", \"password\":\"vncpassword\"}", VNCPort)
+		fmt.Fprintf(httpResponse, "{\"portNumber\":\"%d\", \"password\":\"vncpassword\"}", VNCPort)
 	})
 
 	fmt.Println("Server starting on :8091...")

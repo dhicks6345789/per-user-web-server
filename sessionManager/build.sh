439fe813f7625c9ceaa800d9bcfb8f1ab4a605db
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-01-26 19:58:33 +0000

    Update build.sh

--- sessionManager/build.sh
+++ sessionManager/build.sh
@@ -8,7 +8,7 @@ systemctl stop PUWSSessionManager
 # Get any required Go mondules.
 go get github.com/moby/moby/client
 go get github.com/moby/moby/api/types/container
-# go get github.com/alexedwards/argon2id
+go get go get golang.org/x/crypto/argon2
 
 # Clear out any previously-compile binary.
 rm sessionManager

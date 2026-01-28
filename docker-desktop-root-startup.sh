917f5a9d811eddb9de4ae40530f8c68d51013aa4
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-01-28 19:52:03 +0000

    Update docker-desktop-root-startup.sh

--- docker-desktop-root-startup.sh
+++ docker-desktop-root-startup.sh
@@ -4,14 +4,16 @@
 # $4=password
 # $5=vncdisplay
 
+# First, create the user's group.
+groupadd -g $3 $1
 # Create the user with a home directory and bash shell.
 useradd -m --uid "$2" --gid "$3" -s /bin/bash "$1"
 # Set the user's password.
-echo "$1:$2" | chpasswd
+echo "$1:$4" | chpasswd
 echo "Created user $1 with IDs $2:$3."
 
 cp /root/docker-desktop-user-startup.sh /home/$1/startup.sh
 chown $1 /home/$1/startup.sh
 chmod u+x /home/$1/startup.sh
 
-su - $1 -c "bash /home/$1/startup.sh"
+su - $1 -c "bash /home/$1/startup.sh $1 $2 $3 $4 $5"

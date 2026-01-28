c21798e130b2eba65365c2cfc18e7d6150411081
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-01-28 17:09:13 +0000

    Update docker-desktop-startup.sh

--- docker-desktop-startup.sh
+++ docker-desktop-startup.sh
@@ -1,7 +1,21 @@
-# Set up VNC password.
-mkdir -p /home/desktopuser/.vnc && echo "$1" | vncpasswd -f > /home/desktopuser/.vnc/passwd && chmod 600 /home/desktopuser/.vnc/passwd
+# $1=username
+# $2=User UID
+# $3=User GID
+# $4=password
+# $5=vncdisplay
 
-echo "Starting VNC server, password $1 on display number $2."
+# Create the user with a home directory and bash shell.
+useradd -m --uid "$2" --gid "$3" -s /bin/bash "$1"
+# Set the user's password.
+echo "$1:$2" | chpasswd
+echo "Created user $1 with IDs $2:$3."
 
-# Start TigerVNC.
-vncserver -fg -localhost no -geometry 1280x720 :$2
+sudo -u $1 bash <<EOF
+  # Set up VNC password.
+  mkdir -p /home/$1/.vnc && echo "$4" | vncpasswd -f > /home/$1/.vnc/passwd && chmod 600 /home/$1/.vnc/passwd
+  
+  echo "Starting VNC server, password $4 on display number $5."
+
+  # Start TigerVNC.
+  vncserver -fg -localhost no -geometry 1280x720 :$5  
+EOF

7420eca07227df9596e4a67afd063085df63c420
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-02-15 16:17:16 +0000

    Update docker-desktop-user-startup.sh

--- docker-desktop-user-startup.sh
+++ docker-desktop-user-startup.sh
@@ -4,9 +4,14 @@
 # $4=password
 # $5=vncdisplay
 
-echo Setting up VNC password.
-mkdir -p /home/$1/.vnc
-echo $4 | vncpasswd -f > /home/$1/.vnc/passwd
+echo Setting up VNC password...
+
+#mkdir -p /home/$1/.vnc
+#echo $4 | vncpasswd -f > /home/$1/.vnc/passwd
+#chmod 600 /home/$1/.vnc/passwd
+
+mkdir -p /home/$1/.vnc/
+printf "$4\n$4\nn\n" | vncpasswd /home/$1/.vnc/passwd
 chmod 600 /home/$1/.vnc/passwd
 
 echo "Starting VNC server, password $4 on display number $5."

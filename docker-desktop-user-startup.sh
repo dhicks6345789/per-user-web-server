26ea05999541ebb43562bf9765d9b2fe34a51dec
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-02-16 11:35:19 +0000

    Update docker-desktop-user-startup.sh

--- docker-desktop-user-startup.sh
+++ docker-desktop-user-startup.sh
@@ -4,6 +4,15 @@
 # $4=password
 # $5=vncdisplay
 
+# Ensure HOME is set.
+export HOME=/home/$1
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_CACHE_HOME="$HOME/.cache"
+
+# Create the xfce4 directory manually just in case.
+mkdir -p $XDG_CONFIG_HOME/xfce4
+
 echo Setting up VNC password...
 
 #mkdir -p /home/$1/.vnc/

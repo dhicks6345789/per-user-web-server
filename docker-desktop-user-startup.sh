825c770202a83bfcc2e6e680beabb80ff07049d0
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-02-18 14:01:52 +0000

    Update docker-desktop-user-startup.sh

--- docker-desktop-user-startup.sh
+++ docker-desktop-user-startup.sh
@@ -4,27 +4,6 @@
 # $4=password
 # $5=vncdisplay
 
-## Set the home directory.
-#export HOME=/home/$1
-#export USER=$1
-##export DISPLAY=:$5
-#export DISPLAY=:1
-#export PORT=5901
-
-mkdir -p /home/$1/.vnc
-echo "$4" | vncpasswd -f > /home/$1/.vnc/passwd
-chmod 600 /home/$1/.vnc/passwd
-
-#cp /root/docker-desktop-xstartup /home/$1/.vnc/xstartup
-#chown $1:$1 /home/$1/.vnc/xstartup
-#chmod u+x /home/$1/.vnc/xstartup
-
-#mkdir -p /home/$1/.config/tigervnc
-#chown -R $1:$1 /home/$1/.config/tigervnc
-#cp /root/docker-desktop-xstartup /home/$1/.config/tigervnc/xstartup
-#chown $1:$1 /home/$1/.config/tigervnc/xstartup
-#chmod u+x /home/$1/.config/tigervnc/xstartup
-
 echo "Starting VNC server, password $4 on display number $5."
 vncserver :$5 -geometry 1280x800 -depth 24
 tail -f /home/$1/.vnc/*.log

2d8ee71f8790b2ef77eb997fc1ae929233184b73
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-02-18 11:46:33 +0000

    Update sessionManager.go

--- sessionManager/sessionManager.go
+++ sessionManager/sessionManager.go
@@ -147,8 +147,10 @@ func main() {
 				http.Error(httpResponse, "No free sessions.", http.StatusInternalServerError)
 				return
 			}
-			VNCPort = possibleVNCPort
-			VNCDisplay := int(VNCPort) - 5900
+			//VNCPort = possibleVNCPort
+			//VNCDisplay := int(VNCPort) - 5900
+			VNCPort = 0
+			VNCDisplay := 5900
 
 			// Make sure there is a user with that username on the host machine so that when we create folders to mount in their desktop image they have the appropriate ownership and permissions.
 			userUIDStr := ""

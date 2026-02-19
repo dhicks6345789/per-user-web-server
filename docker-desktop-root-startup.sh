5249fe2aa848b7108d91287b036fb94bbcc87158
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-02-19 17:45:48 +0000

    Update docker-desktop-root-startup.sh

--- docker-desktop-root-startup.sh
+++ docker-desktop-root-startup.sh
@@ -36,10 +36,8 @@ chmod 600 /home/$1/.vnc/passwd
 
 
 
-# Set up the user startup script.
+# Set up and run the user startup script, as the user.
 cp /root/docker-desktop-user-startup.sh /home/$1/startup.sh
 chown $1:$1 /home/$1/startup.sh
 chmod u+x /home/$1/startup.sh
-
-# Run the user startup script as the user.
 su - $1 -c "bash /home/$1/startup.sh $1 $2 $3 $4 $5"

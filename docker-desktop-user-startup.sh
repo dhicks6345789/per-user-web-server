e0881d8a511db06d47d69f7b22dba1ff4796f53f
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-02-19 19:03:27 +0000

    Update docker-desktop-user-startup.sh

--- docker-desktop-user-startup.sh
+++ docker-desktop-user-startup.sh
@@ -5,5 +5,6 @@
 # $5=vncdisplay
 
 echo "Starting VNC server, password $4 on display number $5."
-vncserver :$5 -geometry 1280x800 -depth 24
+#vncserver :$5 -geometry 1280x800 -depth 24
+vncserver :$5 -depth 24
 tail -f /home/$1/.vnc/*.log

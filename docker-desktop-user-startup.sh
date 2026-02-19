64a1766e63c8658fbd1a0f66976a0869ebe2603e
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-02-19 20:55:19 +0000

    Update docker-desktop-user-startup.sh

--- docker-desktop-user-startup.sh
+++ docker-desktop-user-startup.sh
@@ -6,5 +6,5 @@
 
 echo "Starting VNC server, password $4 on display number $5."
 #vncserver :$5 -geometry 1280x800 -depth 24
-vncserver :$5 -depth 24 -PasswordFile /home/$1/.vnc/passwd
+/usr/bin/Xtigervnc :1 -rfbport 5901 -PasswordFile /home/$1/.vncpasswd -SecurityTypes VncAuth -auth /home/d.hicks/.Xauthority -geometry 1920x1200 -depth 24
 tail -f /home/$1/.vnc/*.log

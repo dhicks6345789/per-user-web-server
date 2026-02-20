d59190963959679d279cc6f0693757dde100a62b
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-02-20 12:44:11 +0000

    Update docker-desktop-user-startup.sh

--- docker-desktop-user-startup.sh
+++ docker-desktop-user-startup.sh
@@ -5,8 +5,9 @@
 # $5=vncdisplay
 
 echo "Starting VNC server, password $4 on display number $5."
-#vncserver :$5 -geometry 1280x800 -depth 24
-/usr/bin/Xtigervnc :1 -rfbport 5901 -PasswordFile /home/$1/.vnc/passwd -SecurityTypes VncAuth -auth /home/d.hicks/.Xauthority -geometry 1920x1200 -depth 24
+tigervncserver -fg -localhost no -geometry 1280x720 :$5
+
+#/usr/bin/Xtigervnc :1 -rfbport 5901 -PasswordFile /home/$1/.vnc/passwd -SecurityTypes VncAuth -auth /home/d.hicks/.Xauthority -geometry 1920x1200 -depth 24
 #tigervncserver :1 -rfbport 5901 -SecurityTypes VncAuth -auth /home/d.hicks/.Xauthority -geometry 1920x1200 -depth 24
 
 ## Start the settings daemon (This MUST run for the panel to know its config).
@@ -17,8 +18,5 @@ echo "Starting VNC server, password $4 on display number $5."
 #xfce4-panel &
 #xfdesktop &
 #xfwm4 &
-
-echo Starting Xfce4...
-exec startxfce4
 
 tail -f /home/$1/.vnc/*.log

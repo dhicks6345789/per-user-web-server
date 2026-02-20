b31e91262339c626ba56a0c82fc0771b599d5db1
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-02-20 14:11:35 +0000

    Update docker-desktop-root-startup.sh

--- docker-desktop-root-startup.sh
+++ docker-desktop-root-startup.sh
@@ -21,37 +21,39 @@ echo "Created user $1 with IDs $2:$3."
 
 
 
-## Set up VNC home folder...
-#mkdir -p /home/$1/.vnc
-#chown $1:$1 /home/$1/.vnc/
-#rm /home/$1/.vnc/*
-
-## ...with the passed-in VNC password (same as their standard user password set above)...
-#echo "$4" | vncpasswd -f > /home/$1/.vnc/passwd
-#chown $1:$1 /home/$1/.vnc/passwd
-#chmod 600 /home/$1/.vnc/passwd
-
-## ...and copy in the XStartup script that starts up the user's desktop environment when they connect via VNC.
-#cp /root/docker-desktop-xstartup /home/$1/.vnc/xstartup
-#chown $1:$1 /home/$1/.vnc/xstartup
-#chmod u+x /home/$1/.vnc/xstartup
-
-
-
 # Set up VNC home folder...
-mkdir -p /home/$1/.config/tigervnc
-chown $1:$1 /home/$1/.config/tigervnc
-rm /home/$1/.config/tigervnc/*
+mkdir -p /home/$1/.vnc
+chown $1:$1 /home/$1/.vnc/
+rm /home/$1/.vnc/*
 
 # ...with the passed-in VNC password (same as their standard user password set above)...
-echo "$4" | tigervncpasswd -f > /home/$1/.config/tigervnc/passwd
-chown $1:$1 /home/$1/.config/tigervnc/passwd
-chmod 600 /home/$1/.config/tigervnc/passwd
+echo "$4" | vncpasswd -f > /home/$1/.vnc/passwd
+chown $1:$1 /home/$1/.vnc/passwd
+chmod 600 /home/$1/.vnc/passwd
 
 # ...and copy in the XStartup script that starts up the user's desktop environment when they connect via VNC.
-cp /root/docker-desktop-xstartup /home/$1/.config/tigervnc/xstartup
-chown $1:$1 /home/$1/.config/tigervnc/xstartup
-chmod u+x /home/$1/.config/tigervnc/xstartup
+cp /root/docker-desktop-xstartup /home/$1/.vnc/xstartup
+chown $1:$1 /home/$1/.vnc/xstartup
+chmod u+x /home/$1/.vnc/xstartup
+
+
+
+## Set up VNC home folder...
+#mkdir -p /home/$1/.config/tigervnc
+#chown $1:$1 /home/$1/.config/tigervnc
+#rm /home/$1/.config/tigervnc/*
+
+## ...with the passed-in VNC password (same as their standard user password set above)...
+#echo "$4" | tigervncpasswd -f > /home/$1/.config/tigervnc/passwd
+#chown $1:$1 /home/$1/.config/tigervnc/passwd
+#chmod 600 /home/$1/.config/tigervnc/passwd
+
+## ...and copy in the XStartup script that starts up the user's desktop environment when they connect via VNC.
+#cp /root/docker-desktop-xstartup /home/$1/.config/tigervnc/xstartup
+#chown $1:$1 /home/$1/.config/tigervnc/xstartup
+#chmod u+x /home/$1/.config/tigervnc/xstartup
+
+
 
 # Set up and run the user startup script, as the user.
 cp /root/docker-desktop-user-startup.sh /home/$1/startup.sh

0c64391d5141cfca59ccea4294780a1f929b485d
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-02-26 14:25:00 +0000

    Update docker-desktop-user-startup.sh

--- docker-desktop-user-startup.sh
+++ docker-desktop-user-startup.sh
@@ -4,5 +4,15 @@
 # $4=password
 # $5=vncdisplay
 
+if [ ! -d "/home/$1/Documents/www" ]; then
+  mkdir -p "/home/$1/Documents/www"
+fi
+
+if [ ! -d "/home/$1/Documents/Hugo" ]; then
+  cd "/home/$1/Documents"
+  hugo new site Hugo
+  cd
+fi
+
 echo "Starting VNC server, password $4 on display number $5."
 tigervncserver -fg -localhost no -geometry 1280x720 :$5

4d2e5e4fca898e71b4f660ecf0cae131c582f313
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-02-27 10:23:27 +0000

    Update docker-classTimetabler-root-startup.sh

--- docker-classTimetabler-root-startup.sh
+++ docker-classTimetabler-root-startup.sh
@@ -1,11 +1,24 @@
 # This script runs as root when the image starts up.
 
-cd /root
-git clone https://github.com/ukfootprint/class-timetabler.git
-#rm -rf /root/class-timetabler/backend/venv
+if [ ! -d "/root/class-timetabler" ]; then
+  cd /root
+  git clone https://github.com/ukfootprint/class-timetabler.git
+  rm -rf /root/class-timetabler/backend/venv
+fi
 
-# Install the Class Timetabler backend.
-#RUN cd /root/class-timetabler/backend && python3 -m venv venv && source venv/bin/activate && pip install -r requirements.txt
+if [ ! -d "/root/class-timetabler/backend/venv" ]; then
+  # Install the Class Timetabler backend.
+  cd /root/class-timetabler/backend
+  python3 -m venv venv
+fi
+
+cd /root/class-timetabler/backend
+source venv/bin/activate
+pip install -r requirements.txt
 
 # Install the Class Timetabler frontend.
-#RUN cd /root/class-timetabler/frontend && npm install
+cd /root/class-timetabler/frontend
+npm install
+
+cd /root/class-timetabler
+./dev.sh

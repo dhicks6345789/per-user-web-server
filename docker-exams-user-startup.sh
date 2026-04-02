e117cfb57dbc44fc8f2249db6ad97c8f54721fbd
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-04-02 17:18:04 +0100

    Create docker-exams-user-startup.sh

---
+++ docker-exams-user-startup.sh
@@ -0,0 +1,8 @@
+# $1=username
+# $2=User UID
+# $3=User GID
+# $4=password
+# $5=vncdisplay
+
+echo "Starting VNC server, password $4 on display number $5."
+tigervncserver -fg -localhost no -geometry 1280x720 :$5

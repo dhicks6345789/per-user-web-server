34e5a10eebea1a6bacd9800174927b2eb18d9f60
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-05-20 14:34:09 +0100

    Update rcloneGUI.go

--- rcloneGUI/rcloneGUI.go
+++ rcloneGUI/rcloneGUI.go
@@ -5,14 +5,15 @@
 package main
 
 import (
-	#"os"
-	#"io"
-	#"log"
-	#"bytes"
-	#"strings"
-	#"net/http"
-	#"net/http/cgi"
-	#"path/filepath"
+	//"os"
+	//"io"
+	"fmt"
+	"log"
+	//"bytes"
+	//"strings"
+	//"net/http"
+	//"net/http/cgi"
+	"path/filepath"
 )
 
 // The root web server folder. Important: don't include include the trailing slash so the prefix gets removed properly from request path strings.
@@ -27,85 +28,21 @@ func fileExists(thePath string) bool {
 	return true
 }
 
-func main() {
+func main() {	
 	// Handle all HTTP request URLs.
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		requestPath := filepath.Clean(r.URL.Path)
 		fullPath := filepath.Join(rootPath, requestPath)
 
-		// If the user asks for the root path, we return the special index file with string substitutions.
-		if requestPath == "" || requestPath == "/" {
-			fullPath = "/var/www/index.html"
-		}
-
-		// Check if the requested path exists on the file system - it might be a file or a folder.
-		requestStatInfo, requestStatErr := os.Stat(fullPath)
-		if os.IsNotExist(requestStatErr) {
-			http.NotFound(w, r)
-			return
-		}
-
-		// If the user has requested a directory, serve any default "index" filesthat might be present there, in order of precident.
-		if requestStatInfo.IsDir() {
-			if fileExists(fullPath + "/" + "index.html") {
-				fullPath = fullPath + "/" + "index.html"
-			} else if fileExists(fullPath + "/" + "index.py") {
-				fullPath = fullPath + "/" + "index.py"
-			}
-			requestStatInfo, _ = os.Stat(fullPath)
-		}
+		fmt.Fprint(w, "Hello, World!")
 
 		// A message for the user / logs.
-		log.Print("wwwServer, request: " + requestPath + ", serving: " + fullPath)
-
-		// Handle CGI scripts (assuming .cgi or .py extension).
-		if !requestStatInfo.IsDir() && (filepath.Ext(fullPath) == ".cgi" || filepath.Ext(fullPath) == ".py") {
-			handleCGI(w, r, fullPath, requestStatInfo)
-			return
-		}
-
-		// Otherwise, serve as a static file.
-		http.ServeFile(w, r, fullPath)
+		log.Print("rcloneGUI, request: " + requestPath + ", serving: " + fullPath)
 	})
 
 	// Execution starts here.
 	log.Println("wwwServer starting on :8080...")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
-	}
-}
-
-// The function that handles CGI scripts.
-func handleCGI(w http.ResponseWriter, r *http.Request, path string, info os.FileInfo) {
-	// We extract the username to run the CGI script as.
-	username := strings.Split(strings.TrimPrefix(path, rootPath+"/"), "/")[0]
-
-	// To do: we might to disallow some usernames here - "root", probably.
-	
-	// We want to capture any error produced by the CGI script so we can display them to the user - this is a CGI server for learners.
-	var errBuf bytes.Buffer
-	
-	// Set up the request's handler.
-	handler := &cgi.Handler{
-		// All scripts run under "sudo" so we can change the username they run as.
-		Path:   "/usr/bin/sudo",
-		Args:   []string{"-u", username, path},
-		Dir:    filepath.Dir(path),
-		Env:    []string{"PATH=/usr/local/bin:/usr/bin:/bin"},
-		// We both capture any error output to display to the user and write it to stderr as normal so it appears in the logs.
-		Stderr: io.MultiWriter(&errBuf, os.Stderr),
-	}
-
-	// Handle the request - hand over to Go's standard library.
-	handler.ServeHTTP(w, r)
-
-	// After execution, check if we caught any errors.
-	if errBuf.Len() > 0 {
-		// We format the error for the user - this is for learners, a nice obvious error message here is a good thing.
-		w.Write([]byte("\n<div>\n"))
-		w.Write([]byte("<pre style=\"background: #2d2d2d; color: #f8f8f2; padding: 15px; border-radius: 5px; overflow-x: auto; font-family: 'Courier New', monospace;\">\n"))
-		w.Write(errBuf.Bytes())
-		w.Write([]byte("\n</pre>\n"))
-		w.Write([]byte("</div>\n"))
 	}
 }

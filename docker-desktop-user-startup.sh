371ca10a6e3532416314d909dddd5f5afd699dfc
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-05-21 13:53:31 +0100

    Update docker-desktop-user-startup.sh

--- docker-desktop-user-startup.sh
+++ docker-desktop-user-startup.sh
@@ -14,5 +14,11 @@ if [ ! -d "/home/$1/Documents/Hugo" ]; then
   cd
 fi
 
+# Run rclone in "GUI" mode as a service. This lets the user connect to a (web based) graphical user interface to use rclone.
+# A separate container provides a per-user proxy for that GUI interface, so users can connect to the rclone GUI via the Pangolin gateway.
+rclone gui --addr localhost:8090 --api-addr localhost:8091 --no-open-browser --no-auth
+#--pass string            Password for RC authentication
+#--user string            User name for RC authentication
+
 echo "Starting VNC server, password $4 on display number $5."
 tigervncserver -fg -localhost no -geometry 1280x720 :$5

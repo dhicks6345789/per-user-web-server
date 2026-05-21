35dd75edbb20b6cc79409fd71342d48a99acea85
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-05-21 19:23:30 +0100

    Update docker-desktop-user-startup.sh

--- docker-desktop-user-startup.sh
+++ docker-desktop-user-startup.sh
@@ -18,7 +18,8 @@ fi
 # A separate container provides a per-user proxy for that GUI interface, so users can connect to the rclone GUI via the Pangolin gateway.
 # We use "0.0.0.0" as the IP address so the rclone application binds to the local network interface and allows connections from other
 # machines (in this case, our rclone proxy container) - if we used "localhost" or "127.0.0.1" only local connections will be accepted.
-rclone rcd --rc-web-gui --rc-addr 0.0.0.0:8090 --rc-web-gui-no-open-browser --rc-user-from-header Remote-User &
+echo "Starting rclone GUI server, username $1, password $4 on port 8090."
+rclone rcd --rc-web-gui --rc-addr 0.0.0.0:8090 --rc-web-gui-no-open-browser --rc-user $1 --rc-pass $4 &
 
 echo "Starting VNC server, password $4 on display number $5."
 tigervncserver -fg -localhost no -geometry 1280x720 :$5

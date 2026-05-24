63a01e3299568425a7333725ecb2ac4f75d86a49
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-05-24 18:16:52 +0100

    Update rcloneGUI.go

--- rcloneGUI/rcloneGUI.go
+++ rcloneGUI/rcloneGUI.go
@@ -152,14 +152,14 @@ func main() {
 	rcloneHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Get the username ("Remote-User" HTTP header value injected by Pangolin).
 		username := strings.Split(r.Header.Get("Remote-User"), "@")[0]
-
+		
 		// Make sure a proxy object to the user's Desktop Docker container (which is where rclone will be running) exists.
 		proxy, exists := rcloneProxies.get(username)
 		if exists == false {
 			rcloneProxies.set(username, "http://desktop-" + username + ":8090")
 			proxy, exists = rcloneProxies.get(username)
 		}
-
+		
 		// Rewrite the URL to remove the "/rclone" prefix.
 		r.URL.Path = strings.TrimPrefix(r.URL.Path, "/rclone")
 		

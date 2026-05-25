f6fad26c522ef83779998dc112b216c2df0b16de
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-05-25 15:43:32 +0100

    Update docker-desktop-user-startup.sh

--- docker-desktop-user-startup.sh
+++ docker-desktop-user-startup.sh
@@ -19,8 +19,7 @@ fi
 # We use "0.0.0.0" as the IP address so the rclone application binds to the local network interface and allows connections from other
 # machines (in this case, our rclone proxy container) - if we used "localhost" or "127.0.0.1" only local connections will be accepted.
 echo "Starting rclone GUI server, username $1, password $4 on port 8090."
-# rclone rcd --rc-web-gui --rc-addr 0.0.0.0:8090 --rc-web-gui-no-open-browser --rc-user $1 --rc-pass $4 --rc-serve &
-rclone rcd --rc-web-gui --rc-addr 0.0.0.0:8090 --rc-web-gui-no-open-browser --rc-user-from-header=$4 --rc-serve &
+rclone rcd --rc-web-gui --rc-addr 0.0.0.0:8090 --rc-web-gui-no-open-browser --rc-user $1 --rc-pass $4 --rc-serve &
 
 echo "Starting VNC server, password $4 on display number $5."
 tigervncserver -fg -localhost no -geometry 1280x720 :$5

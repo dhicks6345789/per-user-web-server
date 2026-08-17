19d821e99152a6d32168b0280c3c0c5bf0627eb6
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2026-08-17 22:05:05 +0100

    Update docker-desktop-user-startup.sh

--- docker-desktop-user-startup.sh
+++ docker-desktop-user-startup.sh
@@ -21,8 +21,8 @@ fi
 echo "Starting rclone GUI server, username $1, password $4 on port 8090."
 rclone rcd --rc-web-gui --rc-addr 0.0.0.0:8090 --rc-web-gui-no-open-browser --rc-user $1 --rc-pass $4 --rc-serve &
 
-if [ -f "~/startup.sh" ]; then
-  bash ~/startup.sh $1 $2 $3 $4 $5
+if [ -f "/home/$1/startup.sh" ]; then
+  HOME=/home/$1 bash /home/$1/startup.sh $1 $2 $3 $4 $5
 fi
 
 echo "Starting VNC server, password $4 on display number $5."
